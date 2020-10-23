2fe29300573bcd40bc5fed9004d178a73ac049ef
Niclas <22919812+niclaszll@users.noreply.github.com>
2020-10-23 10:54:03 +0200

    correctly zip folders and move to correct directory

--- wgtfy.py
+++ wgtfy.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 
 import zipfile
+import shutil
 import os
 
-zip_file_name = os.path.basename(os.getcwd()) + ".zip"
+zip_file_name = os.path.dirname(os.getcwd()) + ".zip"
 
 # get files from directory
 list_files = [f for f in os.listdir(".") if os.path.isfile(f)]
 
-# zip files
 with zipfile.ZipFile(zip_file_name, "w") as zipF:
-    for file in list_files:
-        zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            zipF.write(os.path.join(root, file), compress_type=zipfile.ZIP_DEFLATED)
 
 # change file ext to .wgt
 pre, ext = os.path.splitext(zip_file_name)
-os.rename(zip_file_name, pre + ".wgt")
+
+wgt_file = os.path.basename(os.getcwd()) + ".wgt"
+
+# rename and move file
+os.rename(zip_file_name, wgt_file)
+
+# move file to correct directory
+shutil.move(wgt_file, os.path.dirname(os.getcwd()))
 
 print("Widget created successfully")
-
